2c00e8c76188ce4c71c9d5041d8a00684f9ef08b
Tiago Bertoldo <berto.hlb2@gmail.com>
2023-03-14 07:09:26 +0100

    start page all projet , avec requete de la base de donne

--- src/Pages/AlllProjets/index.jsx
+++ src/Pages/AlllProjets/index.jsx
@@ -1,21 +1,29 @@
 import { useParams } from "react-router"
 import { infoProjet } from "../../components/bd_json"
-
+import SideBar from "../../components/Sidebar"
+import './Styles/desktop.scss'
+import './Styles/mobile.scss'
 
 export default function AllProjets() {
     let {idProjet} = useParams()
     // let isProjet = infoProjet.find((idProjet) => idProjet.id === idProjet);
-    
 
     return(
-        <>
-           {infoProjet.map((element) => (
-               element.id === idProjet ?  (
-                <div key={element.id}>
-                    {element.nom}
+        <main id="all-projets-container">
+        <SideBar/>
+            <section className="container-projets">
+                <div className="header-projets">
+                    {infoProjet.map((element) => (
+                        element.id === idProjet ?  (
+                            <div key={element.id}>
+                                <h2>Projet <span>{element.nom}</span></h2>
+                            </div>
+                        ): null
+                    ))}
                 </div>
-               ): null
-           ))}
-        </>
+                
+            </section>
+            
+        </main>
     )
 }
